f443fadf774dfe94d32f293d16db22c4f3beeb3f
Lefan <tan6@ualberta.ca>
2020-01-18 12:39:59 -0700

    added octaves and stuff to noise.cs

--- FloatingIslands/Assets/Assets/Scripts/Noise.cs
+++ FloatingIslands/Assets/Assets/Scripts/Noise.cs
@@ -3,20 +3,47 @@ using System.Collections;
 
 public static class Noise {
 
-	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale) {
+	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int octaves, int persistance, int lacunarity) {
 		float[,] noiseMap = new float[mapWidth,mapHeight];
 
 		if (scale <= 0) {
 			scale = 0.0001f;
 		}
 
+		float maxNoiseHeight = float.MaxValue;
+		float minNoiseHeight = float.MinValue;
+
 		for (int y = 0; y < mapHeight; y++) {
 			for (int x = 0; x < mapWidth; x++) {
-				float sampleX = x / scale;
-				float sampleY = y / scale;
 
-				float perlinValue = Mathf.PerlinNoise (sampleX, sampleY);
-				noiseMap [x, y] = perlinValue;
+					float amptitude = 1;
+					float frequency = 1;
+					float noiseHeight = 1;
+
+				for (int i = 0; i < octaves; i++){
+					float sampleX = x / scale;
+					float sampleY = y / scale;
+
+					float perlinValue = Mathf.PerlinNoise (sampleX, sampleY) * 2 - 1;	
+					noiseHeight += perlinValue *  amptitude;
+
+					amptitude *= persistance;
+					frequency *= lacunarity;
+				}
+
+				if(noiseHeight > maxNoiseHeight){
+					maxNoiseHeight = noiseHeight;
+				}else if(noiseHeight < minNoiseHeight){
+					minNoiseHeight = noiseHeight;
+				}
+				noiseMap[x, y] = noiseHeight;
+			}
+		}
+
+		//normalize noise map to value between 0 and 1
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = 0; x < mapWidth; x++) {
+				noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
 			}
 		}
 
