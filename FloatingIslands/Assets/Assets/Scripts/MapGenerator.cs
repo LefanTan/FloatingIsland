73cd544e054f6584512b66b2977190ef2cd47b75
Lefan <tan6@ualberta.ca>
2020-01-18 13:13:32 -0700

    persistance and lacunarity fully added
    to noise.cs

--- FloatingIslands/Assets/Assets/Scripts/MapGenerator.cs
+++ FloatingIslands/Assets/Assets/Scripts/MapGenerator.cs
@@ -7,17 +7,35 @@ public class MapGenerator : MonoBehaviour {
 	public int mapHeight;
 	public float noiseScale;
 	public int octaves;
-	public float persistance;
-	public float lucanarity;
+	[Range(0,1)]
+	public float persistance; //affects amptitude so that amptitude decreases with octaves
+	public float lacunarity; //affects frequency so that frequency increases with octaves 
+	public int seed;
+	public Vector2 offset;
 
 	public bool autoUpdate;
 
 	public void GenerateMap() {
-		float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, noiseScale, octaves, persistance, lucanarity);
+		float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
 
 		MapDisplay display = FindObjectOfType<MapDisplay> ();
 		display.DrawNoiseMap (noiseMap);
 	}
 	
+	void OnValidate(){
+		if(mapWidth < 1){
+			mapWidth = 1;
+		}
+		if(mapHeight < 1){
+			mapHeight = 1;
+		}
+		if(octaves < 0){
+			octaves = 0;
+		}
+		if(lacunarity < 1 ){
+			lacunarity = 1;
+		}
+	}
+
 }
--- FloatingIslands/Assets/Assets/Scripts/Noise.cs
+++ FloatingIslands/Assets/Assets/Scripts/Noise.cs
@@ -3,8 +3,17 @@ using System.Collections;
 
 public static class Noise {
 
-	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale, int octaves, float persistance, float lacunarity) {
+	public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset) {
 		float[,] noiseMap = new float[mapWidth,mapHeight];
+
+		System.Random rng = new System.Random(seed);
+		Vector2[] octaveOffsets = new Vector2[octaves];
+
+		for(int i = 0; i < octaves; i++){
+			float offsetX = rng.Next(-100000, 100000) + offset.x;
+			float offsetY = rng.Next(-100000, 100000) + offset.y;
+			octaveOffsets[i] = new Vector2(offsetX, offsetY);
+		}
 
 		if (scale <= 0) {
 			scale = 0.0001f;
@@ -16,7 +25,6 @@ public static class Noise {
 		float halfWidth = mapWidth / 2f;
 		float halfHeight = mapHeight / 2f;
 
-
 		for (int y = 0; y < mapHeight; y++) {
 			for (int x = 0; x < mapWidth; x++) {
 		
@@ -25,8 +33,8 @@ public static class Noise {
 				float noiseHeight = 0;
 
 				for (int i = 0; i < octaves; i++) {
-					float sampleX = x / scale * frequency;
-					float sampleY = y/ scale * frequency;
+					float sampleX = (x - halfWidth) / scale * frequency + octaveOffsets[i].x;
+					float sampleY = (y - halfHeight) / scale * frequency +octaveOffsets[i].y;
 
 					float perlinValue = Mathf.PerlinNoise (sampleX, sampleY) * 2 - 1;
 					noiseHeight += perlinValue * amplitude;
