33804fd4e7075b4e1eacfcc6aeb0db61e966d719
Tom Maurer <tom.c.maurer@gmail.com>
2020-01-18 13:16:31 -0700

    colores

--- FloatingIslands/Assets/Assets/Scripts/MapGenerator.cs
+++ FloatingIslands/Assets/Assets/Scripts/MapGenerator.cs
@@ -3,18 +3,50 @@ using System.Collections;
 
 public class MapGenerator : MonoBehaviour {
 
+	public enum DrawMode {
+		Noise,
+		Color
+	}
+	public DrawMode drawMode;
 	public int mapWidth;
 	public int mapHeight;
 	public float noiseScale;
 
 	public bool autoUpdate;
+	public TerrainType[] regions;
 
 	public void GenerateMap() {
-		float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, noiseScale);
+		float[,] heightMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, noiseScale);
+		Color[] colorMap = new Color[mapWidth * mapHeight];
 
+		for (int y = 0; y < mapHeight; y++){
+			for (int x = 0; x < mapWidth; x ++){
+				float currentHeight = heightMap[x, y];
+				for (int i = 0; i < regions.Length; i ++) {
+					if (currentHeight < regions[i].height){
+						colorMap[y * mapWidth + x] = regions[i].color;
+						break;
+					}
+				}
+			}
+		}
 
-		MapDisplay display = FindObjectOfType<MapDisplay> ();
-		display.DrawNoiseMap (noiseMap);
+		if (drawMode == DrawMode.Noise){
+			MapDisplay display = FindObjectOfType<MapDisplay> ();
+			display.DrawTexture(TextureGenerator.TextureFromHeightMap(heightMap));
+		} else if (drawMode == DrawMode.Color){
+			MapDisplay display = FindObjectOfType<MapDisplay>();
+			display.DrawTexture(TextureGenerator.TextureFromColorMap(colorMap, mapWidth, mapHeight));
+		}
+
+		
 	}
 	
+}
+
+[System.Serializable]
+public struct TerrainType {
+	public string name;
+    public float height;
+    public Color color;
 }
