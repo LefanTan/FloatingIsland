fa00f1a6f0c29e5d1973ad8136f9dbca21694c0d
Lefan <tan6@ualberta.ca>
2020-01-18 18:00:01 -0700

    noise ;)

---
+++ FloatingIslands/Assets/Editor/RegionEditor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MapGenerator))]
+public class RegionEditor : Editor
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
